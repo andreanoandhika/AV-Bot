a68e26c2a6c5d9c7fee350d32d8f842836b52811
andreanoandhika <31299652+andreanoandhika@users.noreply.github.com>
2018-03-06 13:23:22 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-  channelAccessToken: "",
-  channelSecret: ""
+  channelAccessToken: "tTdv8Lj6UlgDCGRs5oF800Pi7EEuvF6IlpdUo5fV7Du3iLioaAUz4WbaxFvgnb5TgUJ2H2zPTIPfCTfRP6xGYE4wwdRRLngh6aBRRLaNL/vj38uwyRn/rAQqLviBAcND5dbjvy6yp4GiEZRpGiJe8gdB04t89/1O/w1cDnyilFU=",
+  channelSecret: "c94d231c042e5d4a5d1fa4260cf7da19"
 };
 
 export default config;
